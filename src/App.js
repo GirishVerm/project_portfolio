23fd1d2eac83674c33628f0f6e9177f3a9ec200b
Girish Verma <78799435+GirishVerm@users.noreply.github.com>
2023-05-02 11:10:09 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import React from "react";
+import {
+  Button,
+  Container,
+  ButtonGroup,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { AppBar } from "@material-ui/core";
 import "./styles.css";
+
+function swaag() {
+  alert("Swag se karenge swaagat");
+}
 
 const App = () => {
   return (
     <div className="App">
-      <header className="Tab-bar">
-        <h1 className="title">My Portfolio</h1>
-        <p className="subtitle">Welcome to my personal website</p>
-      </header>
+      <AppBar position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              LOGO
+            </Typography>
+            <ButtonGroup variant="text" aria-label="text button group">
+              <Button>One</Button>
+              <Button>Two</Button>
+              <Button>Three</Button>
+            </ButtonGroup>
+          </Toolbar>
+        </Container>
+      </AppBar>
     </div>
   );
 };
